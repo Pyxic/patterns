48ebab979aebb5f36b252dd34411fceb71d6e778
Misha Kalita <mishakalita3@gmail.com>
2021-05-05 21:36:58 +0300

    database singleton

--- singleton.py
+++ singleton.py
@@ -1,3 +1,6 @@
+import sqlite3
+
+
 class Singleton:
     _instance = None
 
@@ -5,3 +8,21 @@ class Singleton:
         if cls._instance is None:
             cls._instance = object.__new__(cls)
         return cls._instance
+
+    def __init__(self):
+        self.connection = None
+
+    def connect(self):
+        print(self.connection)
+        if self.connection is None:
+            self.connection = sqlite3.connect("db.sqlite3")
+            self.cursorobj = self.connection.cursor()
+        return self.cursorobj
+
+
+a = Singleton()
+b = Singleton()
+c = Singleton()
+a.connect()
+b.connect()
+c.connect()
