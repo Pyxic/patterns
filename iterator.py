bcf6dc5a90276c33bf857a3ea5b9964132c80cb2
Misha Kalita <mishakalita3@gmail.com>
2021-05-13 21:04:05 +0300

    iterator

---
+++ iterator.py
@@ -0,0 +1,48 @@
+from collections.abc import Iterator, Iterable
+
+
+class AlphabeticalOrderIterator(Iterator):
+    _position = None
+    _reverse = False
+
+    def __init__(self, collection, reverse=False):
+        self._collection = collection
+        self._reverse = reverse
+        self._position = -1 if reverse else 0
+
+    def __next__(self):
+        try:
+            value = self._collection[self._position]
+            self._position += -1 if self._reverse else 1
+        except IndexError:
+            raise StopIteration()
+
+        return value
+
+
+class WordsCollection(Iterable):
+    def __init__(self, collection=[]):
+        self._collection = collection
+
+    def __iter__(self):
+        return AlphabeticalOrderIterator(self._collection)
+
+    def get_reverse_iterator(self):
+        return AlphabeticalOrderIterator(self._collection, True)
+
+    def add_item(self, item):
+        self._collection.append(item)
+
+
+collection = WordsCollection()
+collection.add_item("First")
+collection.add_item("Second")
+collection.add_item("Third")
+
+print("Straight traversal:")
+print("\n".join(collection))
+print("")
+
+print("Reverse traversal:")
+print("\n".join(collection.get_reverse_iterator()), end="")
+
